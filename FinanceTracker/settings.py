0f905d629a646aec56f67dd47f785dcb465b6fba
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-02-27 05:10:06 -0800

    factor out base.html

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'FinanceTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,6 +76,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates/')
 ]
 
 WSGI_APPLICATION = 'FinanceTracker.wsgi.application'

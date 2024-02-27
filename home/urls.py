b7c8631ba5a9ed21f6f780470a41f8ec9c688852
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-02-27 04:50:13 -0800

    implemented logout auth view

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.main, name="main"),
     #path("login", views.login, name="login")
     path("login/", auth_views.LoginView.as_view(template_name="home/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="home/homepage.html"), name="logout"),
 ]

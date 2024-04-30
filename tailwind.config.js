51c6d1380ddfdb65b600136e9211f81e29c00bfc
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-04-30 02:23:34 -0700

    implement portfolio listing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    './**/*.{html,js}',
+    './portfolio/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },

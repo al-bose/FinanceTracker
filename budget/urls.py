a7fc07424c8634e571c43af9787e1cc4a382e314
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-05-04 16:48:35 -0700

    initial chart.js implementation

--- budget/urls.py
+++ budget/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.main, name="main"),
     path('createexpense', views.createExpense, name='createexpense'),
     path("updateexpense", views.updateExpense, name="updateexpense"),
-    path("deleteexpense", views.deleteExpense, name="deleteexpense")
+    path("deleteexpense", views.deleteExpense, name="deleteexpense"),
+    path("createchartdata", views.createChartData, name="createchartdata")
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Expenses
 from django.urls import reverse
 from decimal import Decimal
+import datetime
+from timedelta import Timedelta
 
 # Create your views here.
 
 @login_required
 def main(request):
-    expenses = Expenses.objects.filter(user_id=request.user.id)
+    #default to showing expenses dating back to last 30 days
+    delta = Timedelta(days=-30)
+    last_month = datetime.datetime.now() + delta
+    expenses = Expenses.objects.filter(user_id=request.user.id).filter(date__gte = last_month)
+
+    data = {}
+
+    for type in Expenses.TYPE_CHOICES.keys():
+        data[type] = 0
+
+    for expense in expenses:
+        data[expense.type] += expense.amount
+
+
     context = {
         'expenses': expenses,
-        'types': Expenses.TYPE_CHOICES
+        'types': Expenses.TYPE_CHOICES,
+        'data': data
         }
 
     return render(request, 'budget/index.html', context)
@@ -54,3 +70,32 @@ def deleteExpense(request):
         existing_expense = eexisting_expense_query.first()
         existing_expense.delete()
     return HttpResponseRedirect(reverse("budget:main")) 
+
+@login_required
+def createChartData(request):
+    print('here')
+
+    chartLabel = "Expenses in Last 30 days"
+
+    delta = Timedelta(days=-30)
+    last_month = datetime.datetime.now() + delta
+    expenses = Expenses.objects.filter(user_id=request.user.id).filter(date__gte = last_month)
+
+    data = {}
+
+    for type in Expenses.TYPE_CHOICES.keys():
+        data[type] = 0
+
+    for expense in expenses:
+        data[expense.type] += expense.amount
+
+    labels = list(data.keys())
+    data = {
+        "Labels": labels,
+        "chartLabel": chartLabel,
+        "chartdata": [data[type] for type in labels]
+    }
+
+    print(data)
+
+    return JsonResponse(data)

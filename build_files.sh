111988e5acde726d824610d0c9ccf834f33f1dcc
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-05-09 07:41:23 -0700

    update build script

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,15 @@
-# build_files.sh
-pip3 install -r requirements.txt
+echo "BUILD START"
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
+pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
+
+echo "BUILD END"

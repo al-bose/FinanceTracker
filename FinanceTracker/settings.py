5d76b3aa607c27558d99fdaabfb6aef566b9d166
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-05-09 16:37:21 -0700

    add screenshots to home page

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -143,9 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
 
 
 LOGIN_REDIRECT_URL = "/"

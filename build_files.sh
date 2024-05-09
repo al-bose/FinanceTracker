5d412655d3588160ce2a1ca31cacb619891c20ff
Amlan Bose <36972360+al-bose@users.noreply.github.com>
2024-05-09 08:08:43 -0700

    update requirements with pip3

--- build_files.sh
+++ build_files.sh
@@ -7,9 +7,10 @@ python3.9 -m venv venv
 source venv/bin/activate
 
 # install all deps in the venv
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
 
 echo "BUILD END"
